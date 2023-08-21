c081e1f582f5872668ebc0ecd3d67d60afda1c21
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-08-21 11:59:54 +0600

    Calculation css added

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,3 +1,8 @@
-function clickHandler(target){
-    console.log(target.childNodes[4])
+function clickHandler(targetedItem){
+    console.log(targetedItem.childNodes[5].innerText)
+    const cartItem = document.getElementById('cart-items')
+    const itemName = targetedItem.childNodes[5].innerText
+    const li = document.createElement('li')
+    li.innerText = itemName
+    cartItem.appendChild(li)
 }
