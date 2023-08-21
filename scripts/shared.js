63b385ba77cdda728a0df09d06b9bcf6a9c14949
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-08-21 12:15:59 +0600

    Calculation WIP

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,8 +1,16 @@
+// Global variable
+let total = 0;
+
+//DIv click handler function
 function clickHandler(targetedItem){
     console.log(targetedItem.childNodes[5].innerText)
-    const cartItem = document.getElementById('cart-items')
-    const itemName = targetedItem.childNodes[5].innerText
-    const li = document.createElement('li')
-    li.innerText = itemName
-    cartItem.appendChild(li)
+    // const cartItem = document.getElementById('cart-items')
+    // const itemName = targetedItem.childNodes[5].innerText
+    // const li = document.createElement('li')
+    // li.innerText = itemName
+    // cartItem.appendChild(li)
+
+    const itemPrice = targetedItem.childNodes[7].innerText.split(" ")[0]
+    total = total + parseFloat(itemPrice)
+    console.log(total.toFixed(2))
 }
